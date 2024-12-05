400485173251e70b54b32ee8eb48a10d0fcc2126
Manoochehrbahramkhan <126709446+Manoochehrbahramkhan@users.noreply.github.com>
2024-12-05 16:41:54 +0330

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'yourdbname',
+        'USER': 'yourusername',
+        'PASSWORD': 'yourpassword',
+        'HOST': 'db',  
+        'PORT': '5432',
+    }
+}
